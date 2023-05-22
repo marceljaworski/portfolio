a562d2cc3265413c1497e2b2577b7355401d1ab2
Marcelearn <jaworskimarcel@gmail.com>
2023-05-22 11:00:06 +0200

    text into data.json

--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -1,16 +1,15 @@
 import avatar from "../images/profil-bild.jpg";
+import data from "../content/data.json"
 export const PageAbout = () => {
 	return (
 		<>
 			<div className="paragraph">
 				<img src={avatar} alt="Avatar" className="avatar"></img>
-				<p>
-					Hallo, ich bin Marcel Jaworski, Webentwickler mit einem Hochschulabschluss in Medienwissenschaften und Erfahrung in Fotografie, Bildbearbeitung und Marketing. Dadurch habe ich ein tiefes Verständnis für visuelle Ästhetik, das Erzählen von Geschichten und das Erzeugen von Emotionen.<br/><br/>
-					All diese Fähigkeiten ermöglichen es mir, visuell ansprechende und hochfunktionale Websites zu gestalten. Mit meinen technischen Kenntnissen in Programmiersprachen wie HTML, CSS und JavaScript setze ich Ihre Ideen in attraktive, intuitive und responsive Websites um.<br/><br/>
-					Mein Ansatz basiert darauf, Ihre spezifischen Bedürfnisse zu verstehen und diese in eine effektive Online-Präsenz umzusetzen. Von der Planung und Konzeption bis hin zur Entwicklung und Implementierung arbeite ich mit Ihnen zusammen, um sicherzustellen, dass Ihre Website Ihre Vision widerspiegelt und eine sinnvolle Verbindung zu Ihrem Publikum herstellt.<br/><br/>
-					Wenn Sie auf der Suche nach einem Webentwickler sind, der audiovisuelle Erfahrung mit starken technischen Fähigkeiten kombiniert, bin ich hier, um Ihnen zu helfen, die Online-Präsenz Ihres Unternehmens auf die nächste Stufe zu heben. Gemeinsam können wir ein digitales Erlebnis schaffen, das einen bleibenden Eindruck hinterlässt.<br/><br/>
-					Abschließend möchte ich noch erwähnen, dass mein Hobby, das Jonglieren, nichts mit Programmierung zu tun hat, mir jedoch hilft, meine Konzentration, Koordination, Disziplin, Anpassungsfähigkeit und Kreativität bei der Lösung von Problemen zu verbessern.
-				</p>
+				{data.pageAbout.paragraph.map((el, i) => {
+					return (
+						<p>{el}</p>
+					)
+				})}
 			</div>
 			
 		</>
