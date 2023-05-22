b50a38e04026df50cce83d910800d39777a34e2d
Marcelearn <jaworskimarcel@gmail.com>
2023-05-22 10:29:04 +0200

    static avatar image

--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -1,7 +1,9 @@
+import avatar from "../images/profil-bild.jpg";
 export const PageAbout = () => {
 	return (
 		<>
 			<div className="paragraph">
+				<img src={avatar} alt="Avatar" className="avatar"></img>
 				<p>
 					Hallo, ich bin Marcel Jaworski, Webentwickler mit einem Hochschulabschluss in Medienwissenschaften und Erfahrung in Fotografie, Bildbearbeitung und Marketing. Dadurch habe ich ein tiefes Verständnis für visuelle Ästhetik, das Erzählen von Geschichten und das Erzeugen von Emotionen.<br/><br/>
 					All diese Fähigkeiten ermöglichen es mir, visuell ansprechende und hochfunktionale Websites zu gestalten. Mit meinen technischen Kenntnissen in Programmiersprachen wie HTML, CSS und JavaScript setze ich Ihre Ideen in attraktive, intuitive und responsive Websites um.<br/><br/>
