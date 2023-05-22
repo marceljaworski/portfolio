ab44e3e667fecf624f37e487db97fc4c2bbca7a5
Marcelearn <jaworskimarcel@gmail.com>
2023-05-22 14:20:38 +0200

    codersrank widgets

--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -1,5 +1,6 @@
 import avatar from "../images/profil-bild.jpg";
-import data from "../content/data.json"
+// import github from "../images/github-142-svgrepo-com.svg";
+import data from "../content/data.json";
 export const PageAbout = () => {
 	return (
 		<>
@@ -7,10 +8,18 @@ export const PageAbout = () => {
 				<header className="headerAbout">
 					<img src={avatar} alt="Avatar" className="avatar"></img>
 					<div className="header-content">
-					<h3>Full Stack Developer</h3>
-					<div className="subtitle">
-						<p>HTML | CSS | JavaScript | Node.js | MongoDB | SQL</p>
-					</div>
+						<h3>Full Stack Developer</h3>
+						<div className="subtitle">
+							<p>HTML | CSS | JavaScript | Node.js | MongoDB | SQL</p>
+						</div>
+						{/* <div className="contacts">
+							<a href="https://github.com/marceljaworski" target="_blank" rel="noopener noreferrer">
+            					<span className="icon">
+            					  <img src={github} alt="GitHub" />
+            					</span>
+            					<span>GitHub</span>
+          					</a>
+						</div> */}
 					</div>
 					
 
--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,7 +1,26 @@
+// import CodersRankActivity from '@codersrank/activity';
+
+// register web component as <codersrank-activity> element
+// window.customElements.define('codersrank-activity', CodersRankActivity);
 export const PageWelcome = () => {
 	return (
 		<>
-            
+            {/* <section>
+          <h2>My Coding Activity & Statistics</h2>
+    
+          <codersrank-summary username="marceljaworski" show-header="false"></codersrank-summary>
+          
+          <codersrank-activity username="marceljaworski" legend="false" labels tooltip step="5" ></codersrank-activity>
+          
+          <codersrank-skills-chart
+              username="marceljaworski"
+              labels
+              tooltip
+              skills="JavaScript,CSS,Less,Vue,Svelte,TypeScript,CSS,HTML"
+              svg-width="1120"
+              svg-height="400"
+            ></codersrank-skills-chart>
+        </section> */}
 			<p>-- page under construction</p>
 		</>
 	);
