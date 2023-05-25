8fe0e670f6e6cd70004dabf9a2d2b3c38dae9bbc
Marcelearn <jaworskimarcel@gmail.com>
2023-05-25 14:31:54 +0200

    AppProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { AppProvider } from './AppContext';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+	<AppProvider>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</AppProvider>
 );
