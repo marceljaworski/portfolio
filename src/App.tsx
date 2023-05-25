01a8ac9f731d275808e2e05b3d949bad0d32ab83
Marcelearn <jaworskimarcel@gmail.com>
2023-05-25 13:11:10 +0200

    navigate to welcome

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
 			</nav>
 
 			<Routes>
-				<Route path="/" element={<Navigate to="/about" replace />} />
+				<Route path="/" element={<Navigate to="/welcome" replace />} />
 				<Route path="/welcome" element={<PageWelcome />} />
 				<Route path="/projects" element={<PageProjects />} />
 				<Route path="/about" element={<PageAbout />} />
