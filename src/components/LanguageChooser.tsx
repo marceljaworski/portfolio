25428f06a04f447438250537651032fbfd49f0c9
Marcelearn <jaworskimarcel@gmail.com>
2023-06-05 12:22:23 +0200

    chooseLanguage styled

--- src/components/LanguageChooser.tsx
+++ src/components/LanguageChooser.tsx
@@ -1,17 +1,29 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AppContext } from '../AppContext';
+import TranslateIcon from '@mui/icons-material/Translate';
+
 
 
 export const LanguageChooser = () => {
     const { languages, language, changeLanguage } = useContext(AppContext);
+    const [showLanguages, setShowLanguages] = useState(false);
 
     const handleChange = (event:React.ChangeEvent<HTMLSelectElement>):void => {
         changeLanguage(event.target.value);
     };
+    const handleShow = () => {
+        setShowLanguages(!showLanguages)
+    }
+  
 
     return (
-        <select value={language} onChange={handleChange}>
-            {languages.map(lang => <option key={lang.value} value={lang.value}>{lang.language}</option>)}
-        </select>
+
+        <div className='languageContainer' onClick={handleShow}>
+            {showLanguages && <select value={language} onChange={handleChange}>
+                {languages.map(lang => <option key={lang.value} value={lang.value}>{lang.language}</option>)}
+            </select>}
+            <TranslateIcon fontSize='small'/>
+        </div>
+   
     );
 }
