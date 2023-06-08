2a84b21509a88cc6ffa1c62d2da7cc008298c5b8
Marcelearn <jaworskimarcel@gmail.com>
2023-06-09 01:39:48 +0200

    palette themeProvider CssBaseline Mui

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,32 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AppProvider } from './AppContext';
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { blue, orange } from "@mui/material/colors";
+
+const theme = createTheme({
+	palette: {
+		mode: "dark",
+		primary: {
+			main: blue[400]
+		},
+		secondary: {
+			main: orange[800]
+		}
+		
+	},
+	
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<AppProvider>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</AppProvider>
+
+	<ThemeProvider theme={theme}>
+		<CssBaseline />
+		<AppProvider>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</AppProvider>
+	</ThemeProvider>	
+		
 );
