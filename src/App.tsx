2aa75e18839fe54ec4b540a38523f722e420d73b
Marcelearn <jaworskimarcel@gmail.com>
2023-06-11 00:10:27 +0200

    Responsive App-Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import './App.scss';
-import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate} from 'react-router-dom';
 import { PageWelcome } from './pages/PageWelcome';
 import { PageProjects } from './pages/PageProjects';
 import { PageAbout } from './pages/PageAbout';
 import { Page404 } from './pages/Page404';
-import { LanguageChooser } from './components/LanguageChooser';
-import { Theme } from './components/Theme'
+// import { LanguageChooser } from './components/LanguageChooser';
+// import { Theme } from './components/Theme';
 import { AppContext } from './AppContext';
 import { useContext } from 'react';
+
+
+import { Navbar } from './components/Navbar/Nabvar';
+
 
 function App() {
 	const { data, lightMode } = useContext(AppContext);
 
+
 	return (
 		<div className={"App "+ (lightMode ? 'light' : 'dark')}>
-			<nav className='nav'>
+			{/* <nav className='nav'>
 				<NavLink to="/welcome"><h1>Marcel Jaworski</h1></NavLink>
 				<div className='nav-items'>
 					<NavLink to="/welcome">{data.navText[0]}</NavLink>
@@ -25,9 +30,15 @@ function App() {
 					<Theme/>
 					<LanguageChooser/>
 				</div>
-			</nav>
+			</nav> */}
+
+
+			<Navbar/>
+			
+
 
 			<Routes>
+
 				<Route path="/" element={<Navigate to="/welcome" replace />} />
 				<Route path="/welcome" element={<PageWelcome />} />
 				<Route path="/projects" element={<PageProjects />} />
---
+++ src/components/Navbar/consts/navItems.tsx
@@ -0,0 +1,35 @@
+import HomeIcon from '@mui/icons-material/Home';
+import LanguageIcon from '@mui/icons-material/Language';
+import PreviewIcon from '@mui/icons-material/Preview';
+import PersonIcon from '@mui/icons-material/Person';
+
+export const navItems = [
+    {
+        id: 0,
+        icon: <HomeIcon />,
+        label: 'Home',
+        route: 'welcome'
+
+    },
+    {
+        id: 1,
+        icon: <PreviewIcon />,
+        label: 'Projects',
+        route: 'projects'
+
+    },
+    {  
+        id: 2,
+        icon: <PersonIcon />,
+        label: 'About',
+        route: 'about'
+
+    },
+    // {
+    //     id: 3,
+    //     icon: <LanguageIcon />,
+    //     label: 'Languages',
+    //     route: 'route'
+
+    // }
+]
