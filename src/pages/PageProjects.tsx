fca17766076ae8c6bf79e8315c156d6e2b02e407
Marcelearn <jaworskimarcel@gmail.com>
2023-07-02 16:34:11 +0200

    text corrections

--- src/pages/PageProjects.tsx
+++ src/pages/PageProjects.tsx
@@ -1,8 +1,10 @@
 import './PageProjects.scss';
-import { Typography } from '@mui/material';
+import { Typography, textFieldClasses } from '@mui/material';
 import { AppContext } from '../AppContext';
 import { useContext } from 'react';
 import boredapp from '../images/profil-bild.jpg';
+import bootstrap from '../images/bootstrap.png'
+import eventplanner from '../images/eventdesktop.png';
 
 
 interface project {
@@ -20,24 +22,38 @@ export const PageProjects = () => {
 
 
 	return (
-		<div className="main main-projects">
-			{/* <p>{data.pageProjects.paragraph}</p> */}
-				{/* {data.pageProjects.projects.map((item: project) => */}
-				<div className='card'>
-					<img className='card-image' src={boredapp} alt="" />
-
-					
-					<Typography variant='h4' component="div">{data.pageProjects.projects[0].title}</Typography>
-					
-						
-					<Typography variant='subtitle1'className='stack'>{data.pageProjects.projects[0].stack}</Typography>
-						
-					<Typography variant='body1'>{data.pageProjects.projects[0].features}</Typography>
-					<a href={data.pageProjects.projects[0].link} target="blank"><button>Visit</button></a>
-				
-				</div>
-				{/* )} */}
-			
+		<div className="main">
+			<Typography>{data.pageProjects.paragraph}</Typography>
+			<div className='main-projects'>
+			<div className='card'>
+				<img className='card-image' src={boredapp} alt="" />
+				<Typography variant='h4' component="div">{data.pageProjects.projects[0].title}</Typography>
+				<Typography variant='subtitle1'className='stack'>{data.pageProjects.projects[0].stack}</Typography>
+				<Typography variant='body1'>{data.pageProjects.projects[0].features}</Typography>
+				<a href={data.pageProjects.projects[0].link} target="blank"><button>Visit</button></a>
+			</div>
+			<div className='card'>
+				<img className='card-image' src={eventplanner} alt="" />
+				<Typography variant='h4' component="div">{data.pageProjects.projects[1].title}</Typography>
+				<Typography variant='subtitle1'className='stack'>{data.pageProjects.projects[1].stack}</Typography>
+				<Typography variant='body1'>{data.pageProjects.projects[1].features}</Typography>
+				<a href={data.pageProjects.projects[1].link} target="blank"><button>Visit</button></a>
+			</div>
+			<div className='card'>
+				<img className='card-image' src={boredapp} alt="" />
+				<Typography variant='h4' component="div">{data.pageProjects.projects[2].title}</Typography>
+				<Typography variant='subtitle1'className='stack'>{data.pageProjects.projects[2].stack}</Typography>
+				<Typography variant='body1'>{data.pageProjects.projects[2].features}</Typography>
+				<a href={data.pageProjects.projects[2].link} target="blank"><button>Visit</button></a>
+			</div>
+			<div className='card'>
+				<img className='card-image' src={boredapp} alt="" />
+				<Typography variant='h4' component="div">{data.pageProjects.projects[3].title}</Typography>
+				<Typography variant='subtitle1'className='stack'>{data.pageProjects.projects[3].stack}</Typography>
+				<Typography variant='body1'>{data.pageProjects.projects[3].features}</Typography>
+				<a href={data.pageProjects.projects[3].link} target="blank"><button>Visit</button></a>
+			</div>
+			</div>
 		</div>
 	);
 };
