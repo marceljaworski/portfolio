c357b3cd4e0cb1f81827622da19baeac82c6be13
Marcelearn <jaworskimarcel@gmail.com>
2023-07-05 18:28:31 +0200

    header text styles

--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -8,43 +8,37 @@ import { Typography } from "@mui/material";
 export const PageAbout = () => {
 	const { data } = useContext(AppContext);
 	return (
-		<>
-			<div className="main main-about">
-				<header className="headerAbout">
-				
-					<img src={avatar} alt="Avatar" className="avatar"></img>
-					<div className="header-content">
-						
-						<Typography variant="h5">{data.pageAbout.developer}</Typography>
-						<div className="subtitle">
-							<p>HTML | CSS | JavaScript | Node.js | MongoDB | MySQL</p>
-						</div>
-						{/* <div className="contacts">
-							<a href="https://github.com/marceljaworski" target="_blank" rel="noopener noreferrer">
-            					<span className="icon">
-            					  <img src={github} alt="GitHub" />
-            					</span>
-            					<span>GitHub</span>
-          					</a>
-						</div> */}
-					</div>
-				</header>
-				<article>
-					<Typography className='page-title' variant="h3">{data.pageAbout.about}</Typography>
-				
-					{data.pageAbout.paragraph.map((el: any, i:any) => {
-						return (
-							<>
-								<p className="about-text"key={i}>{el}</p><br/>
-							</>
-							
-						)
-					})}
-				
-
-				</article>
-			</div>
+		<div className="main main-about">
+			<header className="headerAbout">
 			
-		</>
+				<img src={avatar} alt="Avatar" className="avatar"></img>
+				<div className="header-content">
+					
+					<Typography variant="h5">{data.pageAbout.developer}</Typography>
+					<div className="subtitle">
+						<p>HTML | CSS | JavaScript | Node.js | MongoDB | MySQL</p>
+					</div>
+					{/* <div className="contacts">
+						<a href="https://github.com/marceljaworski" target="_blank" rel="noopener noreferrer">
+						<span className="icon">
+						<img src={github} alt="GitHub" />
+						</span>
+						<span>GitHub</span>
+						</a>
+					</div> */}
+					<Typography className='page-title' variant="h4">{data.pageAbout.about}</Typography>
+				</div>
+			</header>
+			<article>
+				{data.pageAbout.paragraph.map((el: any, i:any) => {
+					return (
+						<>
+							<p className="about-text"key={i}>{el}</p><br/>
+						</>
+						
+					)
+				})}
+			</article>
+		</div>
 	);
 };
